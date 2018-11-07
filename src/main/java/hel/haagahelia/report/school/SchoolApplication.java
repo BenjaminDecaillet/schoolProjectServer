d30f74d1b47a74fa55c8e62b2df4ddbf96c830c4
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2018-11-07 18:03:25 +0200

    command line runer comented

--- src/main/java/hel/haagahelia/report/school/SchoolApplication.java
+++ src/main/java/hel/haagahelia/report/school/SchoolApplication.java
@@ -31,48 +31,48 @@ public class SchoolApplication {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(){
-		return args -> {
-			// Save demo data to database
-			// username: kevin password: user
-			Student student1 = new Student("kevin","$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi","kevin@mail.com","USER");
-			// username: ben password: admin
-			Student student2 = new Student("ben","$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG","ben@mail.com","USER");
-			studentRepository.save(student1);
-			studentRepository.save(student2);
-			Grade grade1 = new Grade("Mid Term exam",1,1);
-			Grade grade2 = new Grade("Final Exam",3,2);
-			Grade grade3 = new Grade("Mid Term exam",5,1);
-			Grade grade4 = new Grade("Final Exam",4,2);
-			Subject BI = new Subject("Business Intelligence",student1);
-			Subject IT = new Subject("Informatique",student1);
-			subjectRepository.save(BI);
-			subjectRepository.save(IT);
-			grade1.setSubject(BI);
-			grade2.setSubject(BI);
-			grade3.setSubject(IT);
-			grade4.setSubject(IT);
-			gradeRepository.save(grade1);
-			gradeRepository.save(grade2);
-			gradeRepository.save(grade3);
-			gradeRepository.save(grade4);
-			Grade grade5 = new Grade("Mid Term exam",2,1);
-			Grade grade6 = new Grade("Final Exam",4,2);
-			Grade grade7 = new Grade("Mid Term exam",3,1);
-			Grade grade8 = new Grade("Final Exam",3,2);
-			Subject ECO = new Subject("Economy",student2);
-			Subject DB = new Subject("Database",student2);
-			subjectRepository.save(ECO);
-			subjectRepository.save(DB);
-			grade5.setSubject(ECO);
-			grade6.setSubject(ECO);
-			grade7.setSubject(DB);
-			grade8.setSubject(DB);
-			gradeRepository.save(grade5);
-			gradeRepository.save(grade6);
-			gradeRepository.save(grade7);
-			gradeRepository.save(grade8);
-		};
-	}
+//	@Bean
+//	CommandLineRunner runner(){
+//		return args -> {
+//			// Save demo data to database
+//			// username: kevin password: user
+//			Student student1 = new Student("kevin","$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi","kevin@mail.com","USER");
+//			// username: ben password: admin
+//			Student student2 = new Student("ben","$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG","ben@mail.com","USER");
+//			studentRepository.save(student1);
+//			studentRepository.save(student2);
+//			Grade grade1 = new Grade("Mid Term exam",1,1);
+//			Grade grade2 = new Grade("Final Exam",3,2);
+//			Grade grade3 = new Grade("Mid Term exam",5,1);
+//			Grade grade4 = new Grade("Final Exam",4,2);
+//			Subject BI = new Subject("Business Intelligence",student1);
+//			Subject IT = new Subject("Informatique",student1);
+//			subjectRepository.save(BI);
+//			subjectRepository.save(IT);
+//			grade1.setSubject(BI);
+//			grade2.setSubject(BI);
+//			grade3.setSubject(IT);
+//			grade4.setSubject(IT);
+//			gradeRepository.save(grade1);
+//			gradeRepository.save(grade2);
+//			gradeRepository.save(grade3);
+//			gradeRepository.save(grade4);
+//			Grade grade5 = new Grade("Mid Term exam",2,1);
+//			Grade grade6 = new Grade("Final Exam",4,2);
+//			Grade grade7 = new Grade("Mid Term exam",3,1);
+//			Grade grade8 = new Grade("Final Exam",3,2);
+//			Subject ECO = new Subject("Economy",student2);
+//			Subject DB = new Subject("Database",student2);
+//			subjectRepository.save(ECO);
+//			subjectRepository.save(DB);
+//			grade5.setSubject(ECO);
+//			grade6.setSubject(ECO);
+//			grade7.setSubject(DB);
+//			grade8.setSubject(DB);
+//			gradeRepository.save(grade5);
+//			gradeRepository.save(grade6);
+//			gradeRepository.save(grade7);
+//			gradeRepository.save(grade8);
+//		};
+//	}
 }
